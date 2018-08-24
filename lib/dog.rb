d68a9465df47262fab905ac93754f032426a58c9
John Martinez <jmartinez729@gmail.com>
2018-08-24 21:01:55 +0000

    automatically backed up by learn

---
+++ lib/dog.rb
@@ -0,0 +1,17 @@
+class Dog 
+  
+  def initialize(breed)
+    @breed = "Mutt"
+  end 
+  
+  def breed=(new_breed)
+    @breed = new_breed
+  end 
+  
+  def breed 
+    @breed 
+  end 
+  
+  
+  
+end 
